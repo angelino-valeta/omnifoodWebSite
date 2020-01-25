7192dd571842c8a39e53e64e0cdb38b1158f956f
Angelino Valeta <angelinovaleta@gmail.com>
2020-01-25 12:27:28 +0100

    reading function: Scroll on buttons

--- src/js/script.js
+++ src/js/script.js
@@ -12,6 +12,14 @@ $(document).ready(function(){
     offset: '60px;'
   });
 
-  
+  /* Scroll on buttons */
+  $('.js--scroll-to-plans').click(function(){
+      $('html, body').animate({scrolltop: $('.js--section-plans').offset()
+      .top}, 1000);
+  });
+  $('.js--scroll-to-start').click(function(){
+    $('html, body').animate({scrolltop: $('.js-section-features').offset()
+    .top}, 1000);
+  });
 
 });

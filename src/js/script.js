09d6e5287fcc7a9fef191537cd7f716034c2221f
Angelino Valeta <angelinovaleta@gmail.com>
2020-01-25 12:44:35 +0100

    reading function: Navigation scroll

--- src/js/script.js
+++ src/js/script.js
@@ -22,4 +22,21 @@ $(document).ready(function(){
     .top}, 1000);
   });
 
+  /* Navigation scroll */
+  $(function(){
+    $('a[href*=#]:not([href=#])').click(function(){
+      if(location.pathname.replace(/^\//,'') == this.pathn.replace(/^\//, '') && location.hostname == this.hostname){
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1)
+      + ']');
+      if(target.length){
+        $('html, body').animate({
+        scrolltop: target.offset().top
+        }, 1000);
+        return false;
+        }
+      }
+    });
+  });
+
 });
